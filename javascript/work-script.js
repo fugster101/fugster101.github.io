f0902eac895eb02b9a83007aaea03b735b9b013d
Furqan Agwan <furqankadri@gmail.com>
2017-11-20 13:31:12 +0000

    Changes to Mobile view
    slight chnages to mobile view and desktop chnages some pixesl to image to make them load faster by the server

--- javascript/work-script.js
+++ javascript/work-script.js
@@ -4,3 +4,7 @@ var userViewHeight = window.innerHeight;
 function goback(){
   window.history.back();
 }
+function popup() {
+    var popup = document.getElementById("popup");
+    popup.classList.toggle("show");
+}

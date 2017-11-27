2b078e4f8f2d6565344c0834e2f0b8f14d839588
Furqan Agwan <furqankadri@gmail.com>
2017-11-27 16:53:50 +0000

    More changes to mobile version of the wesbite added a to the top button.

--- javascript/about-script.js
+++ javascript/about-script.js
@@ -1,1 +1,12 @@
-
+window.onscroll = function() {scrollFunction()};
+function scrollFunction(){
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("topButton").style.display = "initial";
+} else {
+    document.getElementById("topButton").style.display = "none";
+}
+}
+function tothetopfucntion(){
+    document.body.scrollTop = 0; // For Safari
+      document.documentElement.scrollTop = 0;
+}
